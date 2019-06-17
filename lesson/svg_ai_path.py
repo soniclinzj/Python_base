5621eadf11a613a50aec0257dab501d9a34a0837
soniclinzj <fjsonic@163.com>
2019-06-17 17:12:05 +0800

    20190617 sonic add lesson ai_path

--- lesson/svg_ai_path.py
+++ lesson/svg_ai_path.py
@@ -1,4 +1,5 @@
 
+import json
 '''
 
 f = open("lesson_ai_path.svg",'r',encoding="utf_8")# 以只读方式打开,加上编码不会产生乱码
@@ -8,7 +9,7 @@ s=f.read()
 path_rule_start = '<path d="'
 path_rule_end = 'z"/>'
 
-open('path_aaa.txt','a+',encoding='utf_8')
+open('path_aaa1.txt','a+',encoding='utf_8')
 
 for k in s:
     print(k)
@@ -27,7 +28,7 @@ def get_str_btw(s, f, b):
     return (par[2].partition(b))[0][:]
 
 
-svg_path = open('path_aaa.txt','a+',encoding='utf_8')
+svg_path = open('path_aaa1.txt','a+',encoding='utf_8')
 tmp = get_str_btw(svg_paths,path_start,path_end)
 print(tmp)
 
@@ -44,22 +45,41 @@ for k in data:
 '''
 
 f = open("lesson_ai_path.svg",'r',encoding="utf_8")# 以只读方式打开,加上编码不会产生乱码
-
 svg_paths=f.read()
-
 path_start = '<path d="'
-path_end = '"/>'
+path_end = '</svg>'
 
-
+#获取两个字符之间的内容：
 def get_str_btw(s, f, b):
-    par = svg_paths.partition(f)
+    par = s.partition(f)
     return (par[2].partition(b))[0][:]
 
-data=[]
-
-svg_path = open('path_aaa.txt','a+',encoding='utf_8')
+svg_path = open('path_aaa.txt','w',encoding='utf_8')
+svg_path_1 = open('path_bbb.txt','w',encoding='utf_8')
 
 tmp = get_str_btw(svg_paths,path_start,path_end)
-data.append(tmp)
+
+tmp1=tmp.replace('"/>','').replace(path_start,'')# 查找并替换
+
+svg_path.write(tmp1) #写入文件
+data = []
+data = tmp1.split(' ')# 依空格进行数据封装
+
+svg_path_1.write(str(data))
 
 print(data)
+
+
+print('\n例二','*'*20)
+
+
+ff = open("info.txt")
+line = ff.readline()
+while line:
+    #dump将字符串转换成字典
+    d1=json.loads(line)
+    print (type(d1))
+    print (d1['item_id'])
+    line = ff.readline()
+
+ff.close()
